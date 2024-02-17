bfa890cc01ff10929fbbc0f966d5fb6ea922a939
luzhihong <xiaopikaqiu@outlook.com>
2024-02-17 17:09:10 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: "Flutter Class",
+    home: Center(
+        child:
+            Text('Hello, world!', textDirection: TextDirection.ltr)), // 文本居中显示
+  ));
+}
